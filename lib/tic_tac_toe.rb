a9a31e39e674f17647474111f1c83ca59c882e83
Dominic Chapman <dominicchapman@me.com>
2017-09-26 23:48:39 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -27,9 +27,9 @@ end
 # board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "\n-----------\n"
+  puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "\n-----------\n"
+  puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
