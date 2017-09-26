720b84ffb733c48466926a3f227bd6ff13c41845
Dominic Chapman <dominicchapman@me.com>
2017-09-26 23:26:30 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -55,3 +55,13 @@ def turn(board)
     turn(board)
   end
 end
+
+def turn_count(board)
+  move_count = 0
+  board.each do |index|
+    if index == "X" || index == "O"
+      move_count += 1
+    end
+  end
+  move_count
+end
