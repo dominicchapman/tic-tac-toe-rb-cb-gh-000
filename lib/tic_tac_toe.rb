796a576df987c548f935d84fadff4095e9626c18
Dominic Chapman <dominicchapman@me.com>
2017-09-26 22:23:22 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,15 @@
 
+WIN_COMBINATIONS = [
+  [0, 1, 2], # horizontal top
+  [3, 4, 5], # horizontal middle
+  [6, 7, 8], # horizontal bottom
+  
+  [0, 3, 6], # vertical left
+  [1, 4, 7], # vertical middle
+  [2, 5, 8], # vertical  right
+  
+  [0, 4, 8], # diagonal left
+  [2, 4, 6]  # diagonal right
+]
+
+
