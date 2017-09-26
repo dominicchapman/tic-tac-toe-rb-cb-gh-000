626ab7953d24ab9d7b58f562ed77c0fae6e6d4b1
Dominic Chapman <dominicchapman@me.com>
2017-09-26 23:28:04 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -65,3 +65,46 @@ def turn_count(board)
   end
   move_count
 end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
+
+# return false/nil if no win combinations present in the board
+# return winning combination indexes as an array if there is a win
+def won?(board)
+    WIN_COMBINATIONS.detect do |combination|
+      # if all parts of a win combination are the same, i.e. X or O or blank
+      board[combination[0]] == board[combination[1]] &&
+      board[combination[1]] == board[combination[2]] &&
+      # and they are not blank, thus X or O
+      position_taken?(board, combination[0])
+      # detect will return first winning combination and nil otherwise
+    end
+end
+
+# return true if every element in the board contains either an "X" or an "O"
+def full?(board)
+  board.all?{|i| i == "X" || i == "O"}
+end
+
+# returns true if the board has not been won and is full
+# returns false if the board is not won and the board is not full
+# returns false if the board is won
+def draw?(board)
+  !won?(board) && full?(board)
+end
+
+# returns true if the board has been won, is a draw, or is full
+def over?(board)
+  won?(board) || draw?(board) || full?(board)
+end
+
+# return the token, "X" or "O" that has won the game given a winning board
+def winner(board)
+  # if there was a winning combination
+  if won?(board)
+    # get the first item in the winning combination (arbitrary) == all other items in the combination
+    board[won?(board)[0]]
+  end
+end
