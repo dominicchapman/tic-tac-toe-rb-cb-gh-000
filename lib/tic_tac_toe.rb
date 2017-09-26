dceeff800dcae89fbe12d5bdebe9922c7b9e9417
Dominic Chapman <dominicchapman@me.com>
2017-09-26 22:26:38 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,13 +3,21 @@ WIN_COMBINATIONS = [
   [0, 1, 2], # horizontal top
   [3, 4, 5], # horizontal middle
   [6, 7, 8], # horizontal bottom
-  
+
   [0, 3, 6], # vertical left
   [1, 4, 7], # vertical middle
   [2, 5, 8], # vertical  right
-  
+
   [0, 4, 8], # diagonal left
   [2, 4, 6]  # diagonal right
 ]
 
 
+# board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "\n-----------\n"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "\n-----------\n"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
